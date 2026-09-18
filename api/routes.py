e35b5944d8a47ca4c8aaed4d4964e9c13365ae6e
rathodfalguni52 <rathodfalguni52@gmail.com>
2026-09-18 13:37:41 +0530

    Connect PyAnalytics engine to FastAPI

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter
+from typing import Any
+from pydantic import BaseModel
+from cleaners.data_cleaner import clean_data
+from analytics.statistics import calculation,group_by_mean
+
+class AnalyzeRequest(BaseModel):
+    data:list[dict[str,Any]]
+
 router=APIRouter()
 
 @router.get("/")
@@ -8,3 +16,18 @@ def root():
 @router.get("/health")
 def health_check():
     return{"status":"healthy"}
+
+@router.post("/analyze")
+def analyze_data(request:AnalyzeRequest):
+    data=request.data
+    cleaned_data=clean_data(data)
+
+    summary=calculation(cleaned_data,"Marks")
+
+    grouped=group_by_mean(cleaned_data,"Department","Marks")
+
+    return{
+        "clean_data":cleaned_data,
+        "Statistical_summary":summary,
+        "average_marks_by_department":grouped
+    }
